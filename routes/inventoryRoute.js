00e3480e050aa04337ee48ac6a92093b2979f113
chinoemma <nuellaemy445@gmail.com>
2025-07-23 13:56:19 -0700

    correction3

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -7,7 +7,7 @@ const invController = require("../controllers/invController");
 const utilities = require("../utilities/")
 
 //Route to uild inventory by  classification view
-//router.get("/type/:classificationId", inController.buildByClassificationId);
+
 router.get("/type/:classificationId", utilities.handleErrors(invController.buildByClassificationId))
 
 router.get("/detail/:inv_id", utilities.handleErrors(invController.buildByInventoryId))
@@ -16,12 +16,12 @@ router.get("/trigger-error", utilities.handleErrors(invController.triggerError))
 
 //intentionally trigger a 500 error
 
-invCont.triggerError = async function (req, res, next) {
-    const error = new Error('this is an intentional 500 error for testing purposes')
-    error.status = 500
-    throw error
+// invCont.triggerError = async function (req, res, next) {
+//     const error = new Error('this is an intentional 500 error for testing purposes')
+//     error.status = 500
+//     throw error
     
-}
+// }
 
 //build inventory item detail view
 
